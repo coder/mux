bb5c0cd21fdf47d3b12752a0d145611ac6b46745
Michael Suchacz <203725896+ibetitsmike@users.noreply.github.com>
2025-11-25 08:26:50 +0000

    feat: add mux version subcommand
    Fixes #586

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,12 +1,18 @@
 #!/usr/bin/env node
 
-const isServer = process.argv.length > 2 && process.argv[2] === "server";
+const subcommand = process.argv.length > 2 ? process.argv[2] : null;
 
-if (isServer) {
+if (subcommand === "server") {
   // Remove 'server' from args since main-server doesn't expect it as a positional argument.
   process.argv.splice(2, 1);
   // eslint-disable-next-line @typescript-eslint/no-require-imports
   require("./server");
+} else if (subcommand === "version") {
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  const { VERSION } = require("../version") as {
+    VERSION: { git_describe: string; git_commit: string };
+  };
+  console.log(`mux ${VERSION.git_describe} (${VERSION.git_commit})`);
 } else {
   // eslint-disable-next-line @typescript-eslint/no-require-imports
   require("../desktop/main");
